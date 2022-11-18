9b463559089e2c2ae4605331f454e9f21b1df547
CreepyGrapy <112834562+CreepyGrapy@users.noreply.github.com>
2022-11-18 09:02:50 -0500

    no rotation

--- test2D/Assets/Script/SpriteLockY.cs
+++ test2D/Assets/Script/SpriteLockY.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class SpriteLockY : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector3 relativePos;// Start is called before the first frame update
+    private Quaternion rotation;
+
     void Start()
     {
+        //Vector3 relativePos = new Vector3(0,1,1);
+        Quaternion rotation=transform.rotation;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        // the second argument, upwards, defaults to Vector3.up
+        transform.rotation = rotation;
         
     }
 }
